536a5c3062fa37931864388745d1289e48792ee1
Selfie SD <webtamil0@gmail.com>
2022-07-08 21:57:21 +0530

    Create cb_buttons.py

---
+++ plugins/cb_buttons.py
@@ -0,0 +1,13 @@
+
+from pyrogram import Client as Clinton
+from plugins.youtube_dl_button import youtube_dl_call_back
+from plugins.dl_button import ddl_call_back
+
+@Clinton.on_callback_query()
+async def button(bot, update):
+
+    cb_data = update.data
+    if "|" in cb_data:
+        await youtube_dl_call_back(bot, update)
+    elif "=" in cb_data:
+        await ddl_call_back(bot, update)
